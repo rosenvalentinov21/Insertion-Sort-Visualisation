74739f6ba488cf14566c69c143278c5c26cafd94
Росен Валентинов <rosen.valentinov21@abv.bg>
2021-12-21 12:48:08 +0200

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,170 @@
+﻿
+namespace Insertion_Sort_Visualizer_2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.buttonEnter = new System.Windows.Forms.Button();
+            this.buttonSort = new System.Windows.Forms.Button();
+            this.buttonClear = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(181, 17);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Input array separated by (,)";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(13, 34);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(226, 22);
+            this.textBox1.TabIndex = 1;
+            // 
+            // buttonEnter
+            // 
+            this.buttonEnter.Location = new System.Drawing.Point(278, 32);
+            this.buttonEnter.Name = "buttonEnter";
+            this.buttonEnter.Size = new System.Drawing.Size(107, 26);
+            this.buttonEnter.TabIndex = 2;
+            this.buttonEnter.Text = "Enter";
+            this.buttonEnter.UseVisualStyleBackColor = true;
+            this.buttonEnter.Click += new System.EventHandler(this.buttonEnter_Click);
+            // 
+            // buttonSort
+            // 
+            this.buttonSort.Location = new System.Drawing.Point(411, 32);
+            this.buttonSort.Name = "buttonSort";
+            this.buttonSort.Size = new System.Drawing.Size(107, 27);
+            this.buttonSort.TabIndex = 3;
+            this.buttonSort.Text = "Sort";
+            this.buttonSort.UseVisualStyleBackColor = true;
+            this.buttonSort.Click += new System.EventHandler(this.buttonSort_Click);
+            // 
+            // buttonClear
+            // 
+            this.buttonClear.Location = new System.Drawing.Point(653, 34);
+            this.buttonClear.Name = "buttonClear";
+            this.buttonClear.Size = new System.Drawing.Size(107, 27);
+            this.buttonClear.TabIndex = 4;
+            this.buttonClear.Text = "Clear";
+            this.buttonClear.UseVisualStyleBackColor = true;
+            this.buttonClear.Click += new System.EventHandler(this.buttonClear_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.White;
+            this.panel1.Controls.Add(this.label4);
+            this.panel1.Controls.Add(this.label3);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.panel1.Location = new System.Drawing.Point(0, 69);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(800, 346);
+            this.panel1.TabIndex = 5;
+            this.panel1.Paint += new System.Windows.Forms.PaintEventHandler(this.panel1_Paint);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label4.ForeColor = System.Drawing.Color.Yellow;
+            this.label4.Location = new System.Drawing.Point(13, 247);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(248, 20);
+            this.label4.TabIndex = 2;
+            this.label4.Text = "The unsorted part of the array ■";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label3.ForeColor = System.Drawing.Color.Red;
+            this.label3.Location = new System.Drawing.Point(13, 211);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(430, 20);
+            this.label3.TabIndex = 1;
+            this.label3.Text = "The element searching for its place in the sorted array ■";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
+            this.label2.Location = new System.Drawing.Point(13, 174);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(230, 20);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "The sorted part of the array ■";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 415);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.buttonClear);
+            this.Controls.Add(this.buttonSort);
+            this.Controls.Add(this.buttonEnter);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button buttonEnter;
+        private System.Windows.Forms.Button buttonSort;
+        private System.Windows.Forms.Button buttonClear;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,156 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Insertion_Sort_Visualizer_2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        int[] Arr = new int[0];
+        TextBox[] myTextboxList = new TextBox[0];
+
+        private void buttonEnter_Click(object sender, EventArgs e)
+        {
+            string arrText = textBox1.Text;
+
+            string[] strArr = arrText.Split(',', '.', '/');
+
+            Arr = new int[strArr.Length];
+            myTextboxList = new TextBox[Arr.Length];
+
+            for (int i = 0; i < strArr.Length; i++)
+            {
+                try
+                {
+                    Arr[i] = int.Parse(strArr[i]);
+                }
+                catch (FormatException)
+                {
+                    MessageBox.Show("Input numbers only,separated by ','  '.' , '/'");
+                }
+            }
+
+            int pointX = 0;
+            int pointY = 50;
+
+            try
+            {
+                for (int i = 0; i < Arr.Length; i++)
+                {
+                    myTextboxList[i] = new TextBox();
+                    myTextboxList[i].Size = new Size(panel1.Width / Arr.Length, 100);
+                    myTextboxList[i].Text = Arr[i].ToString();
+                    myTextboxList[i].Name = i.ToString();
+                    myTextboxList[i].BackColor = Color.Yellow;
+                    myTextboxList[i].Location = new Point(pointX, pointY);
+                    myTextboxList[i].ReadOnly = true;
+                    this.panel1.Controls.Add(myTextboxList[i]);
+                    pointX += panel1.Width / Arr.Length;
+                }
+                panel1.Update();
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show("Exception " + ex);
+            }
+        }
+
+        public void wait(int milliseconds)
+        {
+            var timer1 = new System.Windows.Forms.Timer();
+            if (milliseconds == 0 || milliseconds < 0) return;
+
+            // Console.WriteLine("start wait timer");
+            timer1.Interval = milliseconds;
+            timer1.Enabled = true;
+            timer1.Start();
+
+            timer1.Tick += (s, e) =>
+            {
+                timer1.Enabled = false;
+                timer1.Stop();
+                // Console.WriteLine("stop wait timer");
+            };
+
+            while (timer1.Enabled)
+            {
+                Application.DoEvents();
+            }
+        }
+
+        public void sort(int[] arr)
+        {
+            try
+            {
+                myTextboxList[0].BackColor = Color.Green;
+                for (int i = 1; i < arr.Length; ++i)
+                {
+                    int key = arr[i];
+                    myTextboxList[i].BackColor = Color.Red;
+                    panel1.Update();
+                    wait(1000);
+                    int j = i - 1;
+
+                    while (j >= 0 && arr[j] > key)
+                    {
+                        myTextboxList[j].BackColor = Color.Red;
+                        arr[j + 1] = arr[j];
+                        myTextboxList[j + 1].Text = myTextboxList[j].Text;
+                        myTextboxList[j + 1].BackColor = Color.Green;
+
+                        if (arr[j] > key)
+                        {
+                            myTextboxList[j].Text = key.ToString();
+                        }
+                        j--;
+
+                        panel1.Update();
+                        wait(1000);
+                    }
+                    arr[j + 1] = key;
+                    myTextboxList[j + 1].Text = key.ToString();
+
+                    for (int k = i; k >= 0; k--)
+                        myTextboxList[k].BackColor = Color.Green;
+                    panel1.Update();
+                    wait(1000);
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("New exeption " + ex);
+            }
+        }
+        private void buttonSort_Click(object sender, EventArgs e)
+        {
+            sort(Arr);
+        }
+
+        private void buttonClear_Click(object sender, EventArgs e)
+        {
+            panel1.Controls.Clear();
+        }
+
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
